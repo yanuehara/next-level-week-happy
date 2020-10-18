04a4b5ca79ab08b5ca1994a01ebbcd2b6a36c1aa
Yan Uehara <yanuehara@hotmail.com>
2020-10-18 20:12:37 -0300

    Routes and OrphanagesController added
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,12 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanages from './models/Orphanage';
 
 import './database/connection';
-import Orphanage from './models/Orphanage';
+
+import routes from './routes';
 
 const app = express()
 
 app.use(express.json())
-
-app.post('/orphanages', async (req, res) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = req.body;
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage);
-
-
-    return res.status(201).json(orphanage);
-});
+app.use(routes);
 
 app.listen(3333);
