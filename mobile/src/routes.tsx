698a07ba3df5b9a8de5458d315b28d151ecbd787
Yan Uehara <yanuehara@hotmail.com>
2020-11-07 00:50:16 -0300

    Adds Header to pages
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -9,18 +9,31 @@ import OrphanageMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
 export default function Routes(){
     return (
         <NavigationContainer>
-            <Navigator screenOptions={ {headerShown: false} }>
+            <Navigator screenOptions={ {headerShown: false, cardStyle:{backgroundColor:'#f2f3f5'} } }>
                 <Screen name="OrphanagesMap" component={OrphanageMap}/>
 
-                <Screen name="OrphanageDetails" component={OrphanageDetails}/>
+                <Screen name="OrphanageDetails" component={OrphanageDetails} options={{
+                    headerShown: true,
+                    header: () => <Header showCancel={false} title="Orfanato"/>
+                }}
+                />
 
-                <Screen name="SelectMapPosition" component={SelectMapPosition}/>
+                <Screen name="SelectMapPosition" component={SelectMapPosition} options={{
+                    headerShown: true,
+                    header: () => <Header title="Selecione no mapa"/>
+                }}
+                />
 
-                <Screen name="OrphanageData" component={OrphanageData}/>
+                <Screen name="OrphanageData" component={OrphanageData} options={{
+                    headerShown: true,
+                    header: () => <Header title="Entre com os dados"/>
+                }}
+                />
             </Navigator>
         </NavigationContainer>
     );
