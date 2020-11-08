29dc6043b38ec331f353847e2d1c592e2717d55b
Yan Uehara <yanuehara@hotmail.com>
2020-11-08 00:18:33 -0300

    Adds functionality to SelectMapPosition and OrphanageData
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -4,7 +4,7 @@ import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 
 import mapMarker from '../images/map-marker.png';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import api from '../../services/api';
 
@@ -20,13 +20,13 @@ export default function OrphanagesMap(){
 
     const [orphanages, setOrphanages] = useState<OrphanageItem[]>([]);
 
-    useEffect( ()=>{
+    useFocusEffect( ()=>{
       api.get('orphanages').then(
         response => {
           setOrphanages(response.data)
         }
       )
-    }, [])
+    });
 
     function navigateToDetails(id: number){
         navigation.navigate('OrphanageDetails', {id});
